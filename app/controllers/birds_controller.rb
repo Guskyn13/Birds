a0eb687484cc9168100cd7566ff5fbecbc8e5dd8
Nathan <n.gusky88@gmail.com>
2021-02-25 10:58:25 -0700

    birds n trees

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,29 @@
+class BirdsController < ApplicationController
+
+   def index
+      @birds = Bird.all
+      render json: @birds
+   end
+
+   def show
+      @bird = Bird.find(params[:id])
+      render json: @bird
+   end
+
+   def create
+      @new_bird = Bird.create(name: params[:name])
+      render json: @new_bird
+   end
+
+   def update
+      @bird = Bird.find(params[:id])
+      @bird.update(name: params[:name])
+      render json: @bird
+   end
+
+   def destroy
+      @bird = Bird.find(params[:id])
+      @bird.destroy
+      render json: @bird
+   end
+end
